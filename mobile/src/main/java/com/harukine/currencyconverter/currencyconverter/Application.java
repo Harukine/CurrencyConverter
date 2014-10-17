8af214fca008c3b7e1d5da64b0494c836287aa23
Jacob <jacobhtnguyen@gmail.com>
2014-10-16 21:49:04 -0400

    fixed RestAdapter RequestFade

--- mobile/src/main/java/com/harukine/currencyconverter/currencyconverter/Application.java
+++ mobile/src/main/java/com/harukine/currencyconverter/currencyconverter/Application.java
@@ -17,7 +17,7 @@ public class Application extends android.app.Application {
                 .setEndpoint("http://api.exchangeratelab.com/api/")
                 .setRequestInterceptor(new RequestInterceptor() {
                     @Override
-                    public void intercept(RequestFacade request) {
+                    public void intercept(RequestFacade request){
                         request.addQueryParam("apikey", "2C5F64E8B7B1073C43EBC50BAE01A587");
                     }
                 }).build();
